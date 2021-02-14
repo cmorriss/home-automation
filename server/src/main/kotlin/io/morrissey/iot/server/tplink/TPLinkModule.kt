e9fff31713d13d433376b7494c80dae4ddd9cfd0
Chris Morrissey <cmorriss@gmail.com>
2021-02-14 15:13:56 -0800

    Adding TPLink Support.

---
+++ server/src/main/kotlin/io/morrissey/iot/server/tplink/TPLinkModule.kt
@@ -0,0 +1,9 @@
+package io.morrissey.iot.server.tplink
+
+import dev.misfitlabs.kotlinguice4.KotlinModule
+
+class TPLinkModule : KotlinModule() {
+    override fun configure() {
+        
+    }
+}
